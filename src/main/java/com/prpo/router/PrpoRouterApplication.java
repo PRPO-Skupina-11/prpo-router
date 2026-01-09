f286a01e2b489b1bbc2d7e31a593036a86851d13
Tomaz Golob <tomaz.golob@mediately.co>
2026-01-09 21:37:52 +0100

    Initial commit

---
+++ src/main/java/com/prpo/router/PrpoRouterApplication.java
@@ -0,0 +1,13 @@
+package com.prpo.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrpoRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrpoRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prpo/router/PrpoRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpo.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrpoRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
